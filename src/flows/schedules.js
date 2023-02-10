c0d064692071a83a0161a9da8d53082318c49272
Alexis Guzman <codingcodax@gmail.com>
2023-02-10 15:17:55 -0600

    feat(flows): create schedules

---
+++ src/flows/schedules.js
@@ -0,0 +1,11 @@
+const { addKeyword, addAnswer } = require('@bot-whatsapp/bot');
+
+const schedules = addKeyword(['horarios', 'horario']).addAnswer([
+  'Nuestro horario es el siguiente:',
+  '',
+  'Lunes a Viernes:  9:00 am - 6:00pm',
+  'Sabado:              9:00 am - 6:00pm',
+  'Domingo:           Cerrado',
+]);
+
+module.exports = schedules;
