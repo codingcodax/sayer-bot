3b5792057bd0386421b74841265ce82fd032c0c9
Alexis Guzman <codingcodax@gmail.com>
2023-02-11 10:57:56 -0600

    feat(flows): update main flow

--- src/flows/main.js
+++ src/flows/main.js
@@ -1,14 +1,20 @@
 const { addKeyword, addAnswer } = require('@bot-whatsapp/bot');
 
-const main = addKeyword(['hola', 'ole', 'alo', 'ola', 'menu']).addAnswer(
-  [
-    'Hola!, bienvenid@ a *Sayer - La Via*',
-    'Recuerda que soy un bot',
-    'Selecciona una de las siguientes opciones.',
-  ],
-  {
-    buttons: [{ body: 'Horarios' }, { body: 'Direccion' }],
-  }
-);
+const main = addKeyword(['hola', 'ole', 'alo', 'ola', 'menu'])
+  .addAnswer([
+    '¡Hola! 👋 Soy un bot 🤖 de *Sayer - La Vía*',
+    'Gracias por contactarnos.',
+  ])
+  .addAnswer('¿Cómo puedo ayudarte?')
+  .addAnswer([
+    'Escribe un número basado en lo que quieras hacer.',
+    'En cualquier momento puedes regresar a este menú principal escribiendo la palabra *menú*.',
+    '',
+    '*1.* Horarios',
+    '*2.* Donde nos ubicamos',
+    '*3.* Métodos de pago',
+    // '4. Consultar un precio',
+    // '5. Info acerca de algun producto',
+  ]);
 
 module.exports = main;
