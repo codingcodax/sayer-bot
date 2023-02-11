4204893454fa05ff4bc31302594d8ff95e773e8a
Alexis Guzman <codingcodax@gmail.com>
2023-02-11 11:05:32 -0600

    fix(flows): update saturday hour

--- src/flows/schedules.js
+++ src/flows/schedules.js
@@ -4,7 +4,7 @@ const schedules = addKeyword(['1', '1.', 'horarios', 'horario']).addAnswer([
   'Nuestro horario es el siguiente:',
   '',
   'Lunes a Viernes:  9:00 am - 6:00pm',
-  'Sabado:              9:00 am - 6:00pm',
+  'Sabado:              9:00 am - 5:00pm',
   'Domingo:           Cerrado',
 ]);
 
